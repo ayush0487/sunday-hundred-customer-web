209f26f4c591f31ab9b05f91e08e8425e0ac58b0
akash-cq <ag244834@gmail.com>
2026-03-30 18:01:19 +0530

    url base change

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://api.sundayhundred.com/api/v1",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
