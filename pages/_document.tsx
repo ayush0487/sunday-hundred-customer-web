3a95e48d4d8233f077028f317390a19a77a1d1de
akash-cq <ag244834@gmail.com>
2026-04-02 23:20:11 +0530

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="Ac1Tkj649VTaCd4NNsGlZJaWB44gSrD4QzS8Tzvy9rA"
+        />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/sundayhundred.jpeg" />
       </Head>
