b1008a4f0a9ae02f0d35b8c398484600deb23ab9
akash-cq <ag244834@gmail.com>
2026-04-05 15:35:33 +0530

    api change

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const serverApi = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://api.sundayhundred.com/api/v1",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
