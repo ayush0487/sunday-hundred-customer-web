107e54ae737b47797b7aaa0a166c89a45585ec3e
akash-cq <ag244834@gmail.com>
2026-04-07 22:47:35 +0530

    fix icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,9 @@ export default function Document() {
         <meta name="apple-mobile-web-app-title" content="Sunday Hundred" />
         <meta name="theme-color" content="#0f172a" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <body>
